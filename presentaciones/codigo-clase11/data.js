2bceae169529c56cfa9fc249160f710eb0011b6e
bellinux <bellinuxcom@gmail.com>
2024-09-12 11:44:51 -0300

    Add files via upload

---
+++ presentaciones/codigo-clase11/data.js
@@ -0,0 +1,352 @@
+var terremotos = [
+    {
+        "Fecha": "24 de julio de 2001 (01:00)",
+        "Nombre": "Tarapacá de 2001",
+        "Lat": -19,
+        "Lon": -70,
+        "Zonas": "I y II regiones",
+        "Magnitud": 6.5,
+        "Tzunami": false,
+        "Muertos": 1
+    },
+    {
+        "Fecha": "18 de abril de 2002 (12:08)",
+        "Nombre": "Copiapó de 2002",
+        "Lat": -27.535,
+        "Lon": -70.586,
+        "Zonas": "II y III regiones",
+        "Magnitud": 6.8,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "18 de junio de 2002 (09:56)",
+        "Nombre": "Coquimbo de 2002",
+        "Lat": -30.805,
+        "Lon": -71.124,
+        "Zonas": "IV región",
+        "Magnitud": 6.7,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "20 de junio de 2003 (09:30)",
+        "Nombre": "Coquimbo de 2003",
+        "Lat": -30.653,
+        "Lon": -71.533,
+        "Zonas": "IV y V regiones",
+        "Magnitud": 6.8,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "2 de mayo de 2004 (00:36)",
+        "Nombre": "Angol de 2004",
+        "Lat": -37.695,
+        "Lon": -73.406,
+        "Zonas": "VII, VIII y IX regiones",
+        "Magnitud": 6.7,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "28 de agosto de 2004 (09:41)",
+        "Nombre": "Curicó de 2004",
+        "Lat": -35.173,
+        "Lon": -70.525,
+        "Zonas": "V hasta VIII región",
+        "Magnitud": 6.6,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "13 de junio de 2005 (18:44)",
+        "Nombre": "Tarapacá de 2005",
+        "Lat": -19.895,
+        "Lon": -69.125,
+        "Zonas": "I región",
+        "Magnitud": 7.9,
+        "Tzunami": false,
+        "Muertos": 11
+    },
+    {
+        "Fecha": "30 de abril de 2006 (15:17)",
+        "Nombre": "Copiapó de 2006",
+        "Lat": -27.017,
+        "Lon": -71.022,
+        "Zonas": "III región",
+        "Magnitud": 6.8,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "21 de abril de 2007 (13:53)",
+        "Nombre": "Aysén de 2007",
+        "Lat": -45.266,
+        "Lon": -72.496,
+        "Zonas": "XI región",
+        "Magnitud": 6.3,
+        "Tzunami": true,
+        "Muertos": 10
+    },
+    {
+        "Fecha": "14 de noviembre de 2007 (12:40)",
+        "Nombre": "Tocopilla de 2007",
+        "Lat": -22.314,
+        "Lon": -70.078,
+        "Zonas": "I y II regiones",
+        "Magnitud": 7.7,
+        "Tzunami": false,
+        "Muertos": 2
+    },
+    {
+        "Fecha": "4 de febrero de 2008 (14:01)",
+        "Nombre": "Iquique de 2008",
+        "Lat": -20.166,
+        "Lon": -70.037,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 6.4,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "13 de noviembre de 2009 (00:05)",
+        "Nombre": "Iquique de 2009",
+        "Lat": -19.394,
+        "Lon": -70.321,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 6.6,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "27 de febrero de 2010 (03:34)",
+        "Nombre": "Cauquenes de 2010",
+        "Lat": -36.29,
+        "Lon": -73.239,
+        "Zonas": "IV, V, VI, VII, XVI, VIII, IX, X, XIV regiones y R.M.",
+        "Magnitud": 8.8,
+        "Tzunami": true,
+        "Muertos": 521
+    },
+    {
+        "Fecha": "11 de marzo de 2010 (11:39)",
+        "Nombre": "Pichilemu de 2010",
+        "Lat": -34.259,
+        "Lon": -71.929,
+        "Zonas": "V, VI, VII, XVI y VIII regiones y R.M.",
+        "Magnitud": 7,
+        "Tzunami": false,
+        "Muertos": 2
+    },
+    {
+        "Fecha": "2 de enero de 2011 (17:20)",
+        "Nombre": "Tirúa de 2011",
+        "Lat": -38.35,
+        "Lon": -73.27,
+        "Zonas": "VIII y IX regiones",
+        "Magnitud": 7.3,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "25 de marzo de 2012 (19:37)",
+        "Nombre": "Constitución de 2012",
+        "Lat": -35.12,
+        "Lon": -72.13,
+        "Zonas": "V a VIII regiones",
+        "Magnitud": 7.2,
+        "Tzunami": false,
+        "Muertos": 2
+    },
+    {
+        "Fecha": "17 de abril de 2012 (00:50)",
+        "Nombre": "Zapallar de 2012",
+        "Lat": -32.625,
+        "Lon": -71.365,
+        "Zonas": "IV, V, VI, VII regiones y R.M.",
+        "Magnitud": 6.8,
+        "Tzunami": false,
+        "Muertos": 2
+    },
+    {
+        "Fecha": "30 de enero de 2013 (17:15)",
+        "Nombre": "Vallenar de 2013",
+        "Lat": -28.06,
+        "Lon": -70.84,
+        "Zonas": "III a VIII regiones",
+        "Magnitud": 6.9,
+        "Tzunami": false,
+        "Muertos": 1
+    },
+    {
+        "Fecha": "16 de marzo de 2014 (18:16)",
+        "Nombre": "Iquique de 2014",
+        "Lat": -19.96,
+        "Lon": -70.81,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 6.8,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "1 de abril de 2014 (20:46)",
+        "Nombre": "Iquique de 2014",
+        "Lat": -19.63,
+        "Lon": -70.86,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 8.3,
+        "Tzunami": true,
+        "Muertos": 7
+    },
+    {
+        "Fecha": "2 de abril de 2014 (23:43)",
+        "Nombre": "Iquique de 2014",
+        "Lat": -20.545,
+        "Lon": -70.418,
+        "Zonas": "XV, I, II y III regiones",
+        "Magnitud": 7.7,
+        "Tzunami": true,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "8 de octubre de 2014 (21:14)",
+        "Nombre": "Isla de Pascua de 2014",
+        "Lat": -32.11,
+        "Lon": -110.77,
+        "Zonas": "V región",
+        "Magnitud": 7.1,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "16 de septiembre de 2015 (19:54)",
+        "Nombre": "Coquimbo de 2015",
+        "Lat": -31.535,
+        "Lon": -71.919,
+        "Zonas": "III, IV, V, VI, VII, XVI, VIII, IX regiones y R.M.",
+        "Magnitud": 8.4,
+        "Tzunami": true,
+        "Muertos": 15
+    },
+    {
+        "Fecha": "25 de diciembre de 2016 (11:22)",
+        "Nombre": "Chiloé de 2016",
+        "Lat": -43.517,
+        "Lon": -74.391,
+        "Zonas": "VIII, IX, XIV, X y XI regiones",
+        "Magnitud": 7.6,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "24 de abril de 2017 (18:38)",
+        "Nombre": "Valparaíso de 2017",
+        "Lat": -33.07,
+        "Lon": -72.382,
+        "Zonas": "IV, V, VI, VII, XVI, VIII regiones y R.M.",
+        "Magnitud": 6.9,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "19 de enero de 2019 (22:32)",
+        "Nombre": "Coquimbo de 2019",
+        "Lat": -30.276,
+        "Lon": -71.364,
+        "Zonas": "III, IV, V, VI y R.M.",
+        "Magnitud": 6.7,
+        "Tzunami": false,
+        "Muertos": 2
+    },
+    {
+        "Fecha": "3 de junio de 2020 (03:35)",
+        "Nombre": "San Pedro de Atacama de 2020",
+        "Lat": -23.247,
+        "Lon": -68.53,
+        "Zonas": "XV, I, II y III regiones",
+        "Magnitud": 6.9,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "17 de julio de 2020 (01:40)",
+        "Nombre": "Iquique de 2020",
+        "Lat": -20.235,
+        "Lon": -70.14,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 5.9,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "1 de septiembre de 2020 (00:09)",
+        "Nombre": "Vallenar de 2020",
+        "Lat": -27.96,
+        "Lon": -71.24,
+        "Zonas": "II, III, IV, V regiones y R.M.",
+        "Magnitud": 7,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "11 de septiembre de 2020 (04:35)",
+        "Nombre": "Quillagua de 2020",
+        "Lat": -21.363,
+        "Lon": -69.907,
+        "Zonas": "XV, I y II regiones",
+        "Magnitud": 6.3,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "23 de enero de 2021 (20:36)",
+        "Nombre": "Shetland del Sur de 2021",
+        "Lat": -61.9,
+        "Lon": -55,
+        "Zonas": "XII región",
+        "Magnitud": 7.1,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "28 de julio de 2022 (00:15)",
+        "Nombre": "Tocopilla de 2022",
+        "Lat": -21.931,
+        "Lon": -70.339,
+        "Zonas": "I, II y III regiones",
+        "Magnitud": 6.1,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "12 de noviembre de 2022 (23:24)",
+        "Nombre": "Lebu de 2022",
+        "Lat": -37.465,
+        "Lon": -73.676,
+        "Zonas": "VII, XVI, VIII, IX y XIV regiones",
+        "Magnitud": 6.2,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "30 de marzo de 2023 (14:34)",
+        "Nombre": "Cobquecura de 2023",
+        "Lat": -35.666,
+        "Lon": -73.497,
+        "Zonas": "VII, XVI y VIII regiones",
+        "Magnitud": 6.3,
+        "Tzunami": false,
+        "Muertos": 0
+    },
+    {
+        "Fecha": "18 de julio de 2024 (21:50)",
+        "Nombre": "San Pedro de Atacama de 2024",
+        "Lat": -23.047,
+        "Lon": -67.782,
+        "Zonas": "XV, I, I, III y IV regiones",
+        "Magnitud": 7.4,
+        "Tzunami": false,
+        "Muertos": 1
+    }
+]
